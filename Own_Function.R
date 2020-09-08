cf63808142b0d95a11d6c811f249e1df902d38d0
Jigyasa Sachdeva <60024764+JigyasaSachdeva@users.noreply.github.com>
2020-09-07 21:47:51 -0500

    Mathematical functions

--- Own_Function.R
+++ Own_Function.R
@@ -17,14 +17,14 @@
 #                      return(object)}
 
 
-#Own average function
+# Average function
 x = c(1,4,2,3,4,4,99)
 my_avg <- function (x)
 {
   a = sum (x)/ length(x)
   return(a)
 }
-
+#Test Case:
 my_avg(x)
 #16.71429
 my_avg(y <- seq(1:5))
@@ -38,12 +38,11 @@ sum_squares = function(a,b)
 }
 a <- c(1,2,3,4,7,8,0)
 b <- seq(1:7)
-
+#Test case:
 sum_squares(a,b)
 # 2   8  18  32  74 100  49
 sum_squares(2,3)
 sum_squares(c(2,3), c(1,5))
-
 
 
 #Evaluating Z- Score
